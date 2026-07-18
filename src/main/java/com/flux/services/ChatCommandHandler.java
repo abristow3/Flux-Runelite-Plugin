41969286afb3ebee5d850affc7b52c9fc76047cf
Harry <harrysmithjohnson1996@gmail.com>
2026-07-18 17:46:00 +0100

    ChatCommandHandler return "Inactive" for inactive events

--- src/main/java/com/flux/services/ChatCommandHandler.java
+++ src/main/java/com/flux/services/ChatCommandHandler.java
@@ -2,6 +2,8 @@ package com.flux.services;
 
 import com.flux.FluxPanel;
 import com.flux.FluxPlugin;
+import com.flux.cards.BotmCard;
+import com.flux.cards.SotwCard;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.runelite.api.Client;
@@ -41,12 +43,13 @@ public class ChatCommandHandler {
 		FluxPanel panel = plugin.getPanel();
 		if (panel.getBotmCard() == null) return;
 
-		String bossName = panel.getBotmCard().getBoss().getName();
+		BotmCard card = panel.getBotmCard();
 		ChatMessageBuilder chatMessageBuilder = new ChatMessageBuilder()
 			.append(ChatColorType.NORMAL)
 			.append("Current Boss of the Month: ")
 			.append(ChatColorType.HIGHLIGHT)
-			.append(bossName);
+			.append(card.isEventActive() ? card.getBoss().getName() : "Inactive");
+
 		replaceChatMessage(chatMessage, chatMessageBuilder.build());
 	}
 
@@ -54,11 +57,13 @@ public class ChatCommandHandler {
 		FluxPanel panel = plugin.getPanel();
 		if (panel.getSotwCard() == null) return;
 
+		SotwCard card = panel.getSotwCard();
 		ChatMessageBuilder chatMessageBuilder = new ChatMessageBuilder()
 			.append(ChatColorType.NORMAL)
 			.append("Current Skill of the Week: ")
 			.append(ChatColorType.HIGHLIGHT)
-			.append(panel.getSotwCard().getSkill().getName());
+			.append(card.isEventActive() ? card.getSkill().getName() : "Inactive");
+
 		replaceChatMessage(chatMessage, chatMessageBuilder.build());
 	}
 
